51687fb211bb6296dcd26ec7628d1a33afe6cb84
patrick.sky <patricksky131@gmail.com>
2025-03-07 16:36:29 +0800

    add --address when convert_checksum_address

--- staking_deposit/cli/convert_checksum_address.py
+++ staking_deposit/cli/convert_checksum_address.py
@@ -7,7 +7,11 @@ from staking_deposit.utils.intl import (
 )
 
 FUNC_NAME = 'convert_checksum_address'
+
 @click.command()
+@click.option('--address', required=True, help='The Ethereum address to convert to checksum format')  # 使用 @click.option 来支持 --address
 @click.pass_context
-def convert_checksum_address(address: str):
-    return to_checksum_address(address)
+def convert_checksum_address(ctx, address: str):
+    checksum_address = to_checksum_address(address)
+    click.echo(f'Checksum address: {checksum_address}')
+    return checksum_address
